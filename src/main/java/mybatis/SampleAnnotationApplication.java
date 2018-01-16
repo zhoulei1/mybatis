07c05583a4b5d7dddf56b7db47a312ba41af3f94
zhoulei <347650170@qq.com>
2018-01-16 10:48:45 +0800

    druid

--- src/main/java/mybatis/SampleAnnotationApplication.java
+++ src/main/java/mybatis/SampleAnnotationApplication.java
@@ -17,7 +17,6 @@ package mybatis;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import mybatis.mapper.CityMapper;
 
--- src/main/java/mybatis/config/MybatisDataSourceConfig.java
+++ src/main/java/mybatis/config/MybatisDataSourceConfig.java
@@ -1,7 +1,8 @@
 package mybatis.config;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,11 @@ public class MybatisDataSourceConfig {
 	
 	//此处使用自动装配的H2数据源,可自定义数据源
 	
+/*	@Primary
+	@Bean
+	public DataSource dataSourceOne(){
+	    return DruidDataSourceBuilder.create().build();
+	}*/
     @Bean("sqlSessionFactory")
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource)
             throws Exception {
